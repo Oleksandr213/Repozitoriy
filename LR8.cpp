77a792ccfbd099478251d85905c476243270c1dc
Oleksandr <bio12345676@gmail.com>
2020-12-11 16:39:42 +0200

    add LR8

---
+++ LR8.cpp
@@ -0,0 +1,58 @@
+#define _USE_MATH_DEFINES M_E  
+#include <iostream>
+#include <stdio.h>
+#include "windows.h"
+#include "math.h" 
+
+using namespace std;
+
+bool polindrom(int a);
+void preparation(float& b, float& x, float& Z, float& y) {
+	cout << "b=";
+	cin >> b;
+	cout << "x=";
+	cin >> x;
+	cout << "Z=";
+	cin >> Z;
+	cout << "y=";
+	cin >> y;
+}
+
+
+
+float math() {
+	float b, x, Z, y;
+	preparation(b, x, Z, y);
+	float ODZ = (fabs(5 * b + (x * x * x)) - cos(Z)) + tan(15);
+	if (ODZ == 0) {
+		return NULL;
+	}
+	return (sqrt(b * b * b * b) + pow(M_E, y - 1) / ODZ);
+}
+
+void num(int b, int* arr, int& k) {
+	while (b != 0) {
+		arr[k] = b % 10;
+		b = 10;
+		k += 1;
+	}
+}
+
+bool polindrom(int a) {
+	int b = abs(a), k = 0, arr[100];
+	num(b, arr, k);
+	for (int i = 0; i < int(k / 2); i++) {
+		if (arr[i] != arr[k - i - 1]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	int a = math();
+	cout << polindrom(a) << a << endl;
+	system("pause");
+	return 0;
+}
