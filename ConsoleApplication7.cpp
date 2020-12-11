21f435d948a84b0320029038e06d0cdbb245d6ed
Oleksandr <bio12345676@gmail.com>
2020-12-11 14:27:44 +0200

    add "change LR-5"

--- ConsoleApplication7.cpp
+++ ConsoleApplication7.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 int main()
 {
-	int n1 = 10, s = 100, m = 50;
+	int n1 = 10, s = 100, m = 50, t;
 	double n2;
 
 	n2 = (n1 * s) / m;
+	t = s - m;
 	cout << "n2=" << n2 << endl;
 
 	system("pause");
